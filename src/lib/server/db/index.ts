2d352dfdf25688de4468581e3e83fa5d2e13ad32
EBIHARA <ebi311@gmail.com>
2025-11-04 23:10:05 +0000

    feat: 環境変数に基づいてローカルデータベース接続をサポート

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -5,7 +5,7 @@ import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development' || process.env.USE_LOCAL_DB === 'true') {
 	neonConfig.fetchEndpoint = (host) => {
 		const [protocol, port] = host === 'db.localtest.me' ? ['http', 4444] : ['https', 443];
 		return `${protocol}://neon-proxy:${port}/sql`;
